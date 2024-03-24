852165968c9b0add1a8a18c9eabc0aa0949da2bc
sahil <sahildhameja13@gmail.com>
2024-03-24 15:03:04 +0530

    rady to deploty backend

---
+++ Config/allowedOrigins.js
@@ -0,0 +1,9 @@
+const allowedOrigins =[
+    'https://www.google.com',
+    'http://127.0.0.1.5500',
+    'http://localhost:3500',
+    'http://localhost:3000',
+    'https://blogweb-eo5o.onrender.com'
+      ];
+
+module.exports = allowedOrigins ;
