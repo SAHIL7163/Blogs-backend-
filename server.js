0b9c38572a4e94dd05e30c63ca6db3bb7610c384
sahil <sahildhameja13@gmail.com>
2024-03-24 15:35:24 +0530

    sever.js change

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 const express =require('express');
 const app =express();
 const  cors =require('cors');
-const corseOptions=require('./config/corsOptions')
+const corseOptions=require('./Config/corsOptions')
 const  {Logger}=require('./middleware/logevent');
 const  errorhandler=require('./middleware/errorhandler');
 const verifyJWT =require('./middleware/verifyJWT');
